852943ebb4f2de29bbfe04dc6ae384337ecf86ba
MohammedSGF <malsaggf0021@stu.kau.edu.sa>
2020-09-03 19:53:25 +0300

    fixing the the duplication bounding box for the same weed, now it return a single bounding box for the weed with the confidance

--- performing_detection/opencv/detect.py
+++ performing_detection/opencv/detect.py
@@ -7,6 +7,12 @@ import os
 
 def getWeeds(net, frame, confi_thresh, NMSthresh, labelsPath, show=False):
 
+    """
+    This function takes a network, frame, confidance threshold, NMS threshold, labels path and show permsion
+    and return the high confidance and low confidance weeds in the frame
+    
+    """
+
     LABELS = open(labelsPath).read().strip().split("\n")
 
     np.random.seed(42)
@@ -62,75 +68,64 @@ def getWeeds(net, frame, confi_thresh, NMSthresh, labelsPath, show=False):
             #To just predect weeds
             if predicted_class == 1:
 
-                if predect > confi_thresh:
+                #rescale the x,y,w and h to the image scale
+                box = grid[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                
+                #use the center (x, y)-coordinates to derive the top and
+                #and left corner of the bounding box
+                x = int(centerX - width / 2)
+                y = int(centerY - height / 2)
 
-                    #rescale the x,y,w and h to the image scale
-                    box = grid[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY, width, height) = box.astype("int")
-                    
-                    #use the center (x, y)-coordinates to derive the top and
-                    #and left corner of the bounding box
-                    x = int(centerX - width / 2)
-                    y = int(centerY - height / 2)
+                #update our list of bounding box coordinates, confidences,
+                #and class IDs
+                boxes.append([x, y, int(width), int(height),centerX ,centerY])
+                confidances.append(float(predect))
+                classIDs.append(predicted_class)
 
-                    #update our list of bounding box coordinates, confidences,
-                    #and class IDs
-                    boxes.append([x, y, int(width), int(height),centerX ,centerY])
-                    confidances.append(float(predect))
-                    classIDs.append(predicted_class)
-
-                elif predect > 0.1:
-                    
-                    #rescale the x,y,w and h to the image scale
-                        box = grid[0:4] * np.array([W, H, W, H])
-                        (centerX, centerY, width, height) = box.astype("int")
-                        
-                        #use the center (x, y)-coordinates to derive the top and
-                        #and left corner of the bounding box
-                        x = int(centerX - width / 2)
-                        y = int(centerY - height / 2)
-
-                        #update our list of bounding box coordinates, confidences,
-                        #and class IDs
-                        unkown_boxes.append([x, y, int(width), int(height),centerX ,centerY])
-                        unkown_confidances.append(float(predect))
-                        unkown_classIDs.append(predicted_class)
+                
 
     #apply non-maxima suppression to suppress weak, overlapping bounding
-    #boxes
-    true_boxes = cv2.dnn.NMSBoxes(boxes, confidances, confi_thresh, NMSthresh)
-    unkown_objects = cv2.dnn.NMSBoxes(unkown_boxes, unkown_confidances, 0, NMSthresh)
+    #boxes                                          
+    true_boxes = cv2.dnn.NMSBoxes(boxes, confidances, 0, NMSthresh) #TODO: Play with confidance parmater to have the best results
+    
 
     if show:
         if len(true_boxes) > 0:
             #loop over the indexes we are keeping
             for i in true_boxes.flatten():
+
                 #extract the bounding box coordinates
                 (x, y) = (boxes[i][0], boxes[i][1])
                 (w, h) = (boxes[i][2], boxes[i][3])
                 (centerX , centerY) = (boxes[i][4], boxes[i][5])
                 cv2.circle(frame, (centerX,centerY) , 5, (0, 0, 255) , 4)
-                true_weeds.append([x, y, int(width), int(height),centerX ,centerY,confidances[i]])
+
+                if confidances[i] > confi_thresh:
+                    true_weeds.append([x, y, int(width), int(height),centerX ,centerY,confidances[i]])
+                else:
+                    weeds.append([x, y, int(width), int(height),centerX ,centerY,confidances[i]])
+
                 #draw a bounding box rectangle and label on the image
                 color = [int(c) for c in COLORS[classIDs[i]]]
                 cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
                 text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidances[i])
                 cv2.putText(frame, text, (x + 5, y + 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
 
-        if len(unkown_objects) > 0:
-            #loop over the indexes we are keeping
-            for i in unkown_objects.flatten():
-                #extract the bounding box coordinates
-                (x, y) = (unkown_boxes[i][0], unkown_boxes[i][1])
-                (w, h) = (unkown_boxes[i][2], unkown_boxes[i][3])
-                (centerX , centerY) = (unkown_boxes[i][4], unkown_boxes[i][5])
-                weeds.append([x, y, int(width), int(height),centerX ,centerY,unkown_confidances[i]])
-                cv2.circle(frame, (centerX,centerY) , 5, (255, 0, 0) , 4)
-                #draw a bounding box rectangle and label on the image
-                color = [int(c) for c in COLORS[unkown_classIDs[i]]]
-                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-                text = "{}: {:.4f}".format(LABELS[unkown_classIDs[i]], unkown_confidances[i])
-                cv2.putText(frame, text, (x + 5, y + 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
+        # if len(unkown_objects) > 0:
+        #     #loop over the indexes we are keeping
+        #     for i in unkown_objects.flatten():
+        #         #extract the bounding box coordinates
+        #         (x, y) = (unkown_boxes[i][0], unkown_boxes[i][1])
+        #         (w, h) = (unkown_boxes[i][2], unkown_boxes[i][3])
+        #         (centerX , centerY) = (unkown_boxes[i][4], unkown_boxes[i][5])
+        #         weeds.append([x, y, int(width), int(height),centerX ,centerY,unkown_confidances[i]])
+        #         cv2.circle(frame, (centerX,centerY) , 5, (255, 0, 0) , 4)
+        #         #draw a bounding box rectangle and label on the image
+        #         color = [int(c) for c in COLORS[unkown_classIDs[i]]]
+        #         cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        #         text = "{}: {:.4f}".format(LABELS[unkown_classIDs[i]], unkown_confidances[i])
+        #         cv2.putText(frame, text, (x + 5, y + 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
 
 
         det = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
@@ -156,7 +151,7 @@ if __name__ == "__main__":
     net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
 
-    frame = cv2.imread('C:\\Users\\MohammedSGF\\Desktop\\Senior Project\\WeedDetec\\Crop_and_weed_detection\\performing_detection\\data\\images\\test_6.jpg')
+    frame = cv2.imread('C:\\Users\\MohammedSGF\\Desktop\\Senior Project\\WeedDetec\\Crop_and_weed_detection\\performing_detection\\data\\images\\test_4.jpg')
     
     #parameters
     confi_thresh = 0.3
